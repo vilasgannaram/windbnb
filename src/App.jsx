032a292081ef69ad42f63d738dc790a62b9ef737
vilas Gannaram <vilasgannaram@gmail.com>
2022-11-07 14:35:54 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-center font-mono font-bold  uppercase leading-3 text-red-900 underline  ">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
